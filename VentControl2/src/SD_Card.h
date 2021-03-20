e0af48b924b68492ed2d5169a951093a9e1a8aea
Ove Niemistö <ove.niemisto@gmail.com>
2021-03-20 10:04:24 +0200

    remove unnecessary extern declaration

--- VentControl2/src/SD_Card.h
+++ VentControl2/src/SD_Card.h
@@ -19,17 +19,6 @@ Arduino Nano	11		12		13		10
 
 *************************************************************************/
 
-// extern Motor motor1;
-// extern DigitalTemp	t_Outside;
-// extern AnalogTemp 	t_Panel;
-// extern AnalogTemp 	t_HeatedAir;
-// extern AnalogTemp	t_Inside;
-// extern CurrentSensor current;
-// extern VoltageSensor voltage;
-
-//bool sd_errorFlag;
-//bool unmountedFlag;
-
 void SD_Card_INIT();
 void SD_log(String date, String time);
 void SD_unmount();
